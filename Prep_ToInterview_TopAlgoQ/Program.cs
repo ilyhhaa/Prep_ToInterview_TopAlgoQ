f8e5e0d16cf3442a14790f59ed911e303fc173eb
Ilya Kunitsky <Ilyhhaa@gmail.com>
2024-04-26 15:09:05 +0300

    "it's was soooohard"

--- Prep_ToInterview_TopAlgoQ/Program.cs
+++ Prep_ToInterview_TopAlgoQ/Program.cs
@@ -1,14 +1,17 @@
 ﻿using System;
+using System.Linq;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
-        Solution solution = new Solution();
+        SolutionRotate solutionRotate = new SolutionRotate();
+      
+        int[] ints =  new int[] { -1, -100, 3, 99 };
+        
+         solutionRotate.Rotate(ints, 2);
 
-        var a = solution.MaxProfit(new int[] { 1, 2, 3, 4, 5 });
-
-        Console.WriteLine(a);
+        Console.WriteLine();
     }
 }
 
@@ -48,3 +51,42 @@ public class Solution
         return profit;
     }
 }
+
+/*Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,4,5,6,7], k = 3
+Output: [5,6,7,1,2,3,4]
+Explanation:
+rotate 1 steps to the right: [7,1,2,3,4,5,6]
+rotate 2 steps to the right: [6,7,1,2,3,4,5]
+rotate 3 steps to the right: [5,6,7,1,2,3,4]
+Example 2:
+
+Input: nums = [-1,-100,3,99], k = 2
+Output: [3,99,-1,-100]
+Explanation: 
+rotate 1 steps to the right: [99,-1,-100,3]
+rotate 2 steps to the right: [3,99,-1,-100]*/
+
+public class SolutionRotate
+{
+    public void Rotate(int[] nums, int k)
+    {
+            k %= nums.Length;
+            int[] copy = new int[nums.Length];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                copy[(i + k) % nums.Length] = nums[i];
+            }
+            for (int i = 0; i < nums.Length; i++)
+            {
+                nums[i] = copy[i];
+            }
+
+    }
+}
+//0-1-2    10-11-12
